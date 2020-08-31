bf3604ba1282673ddf05801eb088425777a72ec6
will zhang <zhanliam21@gmail.com>
2020-08-31 16:40:23 -0400

    choose city weather

--- Node/WeatherProject/app.js
+++ Node/WeatherProject/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const https = require("https");
-
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    const query = "London"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + ",uk&appid=d4687dac958f47ff8ebe174b0357c73e&units=metric";
+    res.sendFile(__dirname + "/index.html");
+
+    
+});
+
+app.post("/", function (req, res) {
+    
+    const query = req.body.cityName;
+    const apiKey = "d4687dac958f47ff8ebe174b0357c73e";
+    const units = "metric";
+
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + ",uk&appid=" + apiKey + "&units=" + units;
+
     https.get(url, function(response) {
         console.log(response.statusCode);
         response.on("data", function(data) {
@@ -15,15 +29,14 @@ app.get("/", function(req, res){
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<h1>The temperature in London is " + temp + " degrees Celcius. </h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius. </h1>");
             res.write(" <p> The weather is currently" + weatherDescription + "</p>");
             res.write("<img src=" + imageURL + ">");
             res.send();
 
         })
     });
-    
-});
+})
 
 
 
