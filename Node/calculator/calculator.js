7cd3f77761cc18664b5e004d64cc630ef8108e52
will zhang <zhanliam21@gmail.com>
2020-08-31 13:05:32 -0400

    bmicalc added
    form - action /bmicalc > get ( > post ( / all action here)

--- Node/calculator/calculator.js
+++ Node/calculator/calculator.js
@@ -1,22 +1,18 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded(
-    {
-        extended:true
-    }
-));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
-}
-);
+});
 
 app.post("/", function (req, res) {
-    
+
 
     console.log(req.body);
     var num1 = Number(req.body.num1);
@@ -24,6 +20,21 @@ app.post("/", function (req, res) {
 
     var result = num1 + num2;
     res.send("Sumnation is " + result);
+})
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+app.post("/bmicalculator", function (req, res) {
+
+    console.log(req.body);
+    var w = parseFloat(req.body.weight);
+    var h = parseFloat(req.body.height);
+
+    var bmi = w / (h * h);
+    res.send("Your BMI is " + bmi);
+
 })
 
 app.listen(3000, function () {
