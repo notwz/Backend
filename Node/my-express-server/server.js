430031e49855d7a01c1165b079d8b54bd82d0611
will zhang <zhanliam21@gmail.com>
2020-08-31 12:56:22 -0400

    backend calc added

--- Node/my-express-server/server.js
+++ Node/my-express-server/server.js
@@ -12,6 +12,10 @@ app.get("/contact", function (req, res) {
     res.send("contact me at: @gmail.com");
 });
 
+app.get("/about", function (req, res) {
+    res.send(" i am will zhang love boba");
+});
+
 app.listen(3000, function () {
     console.log("server started on port 3000");
 
