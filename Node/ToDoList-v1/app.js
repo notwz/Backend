292e8c9557f0cf35ebe664237fdda126e0473913
will zhang <zhanliam21@gmail.com>
2020-09-05 23:44:56 -0400

    first ejs daytype

--- Node/ToDoList-v1/app.js
+++ Node/ToDoList-v1/app.js
@@ -3,8 +3,21 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "./index.html")
+    //res.sendFile(__dirname + "./index.html");
+
+    var currentDay = new Date().getDay();
+    var day= "";
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    }
+    else {
+        day = "Weekday";
+    }
+    res.render("list", {kindOfDay: day});
+
 });
 
 app.listen(3000, function(){
