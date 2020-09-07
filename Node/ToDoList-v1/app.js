d4d67176fe565eae28ea74315a5992152545fb0d
will zhang <zhanliam21@gmail.com>
2020-09-06 23:02:41 -0400

    date reformat via toLocaleDateString

--- Node/ToDoList-v1/app.js
+++ Node/ToDoList-v1/app.js
@@ -8,34 +8,16 @@ app.set('view engine', 'ejs')
 app.get("/", function (req, res) {
     //res.sendFile(__dirname + "./index.html");
 
-    var currentDay = new Date().getDay();
-    var day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
+    var today = new Date();
 
-        default:
-            break;
-    }
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    
+    var day = today.toLocaleDateString("en-US", options);
+    
     res.render("list", {
         kindOfDay: day
     });
