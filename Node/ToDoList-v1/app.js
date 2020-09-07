4c6eda1b977bd45adeb7a3ababe882682c96ba2c
will zhang <zhanliam21@gmail.com>
2020-09-07 10:52:12 -0400

    ejs templates
    <%- header footer

--- Node/ToDoList-v1/app.js
+++ Node/ToDoList-v1/app.js
@@ -4,8 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 var items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
-app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 
@@ -23,17 +27,44 @@ app.get("/", function (req, res) {
     };
 
     let day = today.toLocaleDateString("en-US", options);
-    
-    res.render("list", {kindOfDay: day,newListItems: items });
+
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
 
 });
 
 app.post("/", function (req, res) {
     let item = req.body.newItem;
-    items.push(item);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
-    res.redirect("/");
+
 });
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+
+})
 
 app.listen(3000, function () {
     console.log("Server on 3000")
