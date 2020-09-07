5dab0683bd86b8e1561ce7c872915c7100d3e7cb
will zhang <zhanliam21@gmail.com>
2020-09-07 10:07:06 -0400

    let styles

--- Node/ToDoList-v1/app.js
+++ Node/ToDoList-v1/app.js
@@ -12,22 +12,22 @@ app.set('view engine', 'ejs');
 app.get("/", function (req, res) {
     //res.sendFile(__dirname + "./index.html");
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
     
     res.render("list", {kindOfDay: day,newListItems: items });
 
 });
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
 
     res.redirect("/");
